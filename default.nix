8ac3bf36dec589c13e5681e6ccea8978f41186a8
Gregory W. Schwartz <ifthenelse2244@gmail.com>
2020-04-23 15:55:31 -0400

    Added default.nix.

---
+++ default.nix
@@ -0,0 +1,75 @@
+# default.nix
+{ compilerVersion ? "ghc865", pkgsLink ? (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/1e90c46c2d98f9391df79954a74d14f263cad729.tar.gz)}:
+let
+  config = { allowBroken = true;
+             allowUnfree = true;
+             packageOverrides = super: let self = super.pkgs; in {
+                Renv = super.rWrapper.override {
+                  packages = with self.rPackages; [
+                    ggplot2
+                    devtools
+                    cowplot
+                    dplyr
+                    jsonlite
+                    edgeR
+                    reshape2
+                  ];
+                };
+              };
+           };
+  pkgs = import pkgsLink { inherit config; };
+  fontsConf = pkgs.makeFontsConf {
+    fontDirectories = [];
+  };
+  compiler = pkgs.haskell.packages."${compilerVersion}";
+  pkg = compiler.developPackage {
+    root = ./.;
+    source-overrides = {
+      inline-r = "0.10.2";
+      terminal-progress-bar = "0.2";
+    };
+    overrides = self: super: (with pkgs.haskell.lib; with pkgs.haskellPackages; {
+      BiobaseNewick = doJailbreak super.BiobaseNewick;
+      birch-beer = dontHaddock super.birch-beer;
+      clustering = dontBenchmark (dontCheck (addExtraLibraries super.clustering [pkgs.Renv]));
+      diagrams-graphviz = doJailbreak super.diagrams-graphviz;
+      differential = addExtraLibraries super.differential [pkgs.Renv];
+      fgl = doJailbreak super.fgl;
+      hmatrix-svdlibc = dontCheck (doJailbreak super.hmatrix-svdlibc);
+      pipes-text = doJailbreak super.pipes-text;
+      streaming-cassava = doJailbreak super.streaming-cassava;
+      typed-spreadsheet = doJailbreak super.typed-spreadsheet;
+    });
+  };
+  # in case your package source depends on any libraries directly, not just transitively.
+  buildInputs = [ pkgs.zlib
+                  pkgs.zlib.dev
+                  pkgs.zlib.out
+                  pkgs.blas
+                  pkgs.liblapack
+                  pkgs.gfortran.cc.lib
+                  pkgs.cairo
+                  pkgs.stdenv
+                  pkgs.gmp
+                  pkgs.gsl
+                  pkgs.gtk2
+                  pkgs.pango
+                  pkgs.graphviz
+                  pkgs.pkg-config
+                  pkgs.fontconfig
+                  pkgs.Renv
+                  pkgs.makeWrapper
+                  pkgs.glibcLocales
+               ];
+in (pkgs.haskell.lib.dontHaddock (pkg.overrideAttrs(attrs: {
+  buildInputs = attrs.buildInputs ++ buildInputs;
+  nativeBuildInputs = attrs.nativeBuildInputs ++ buildInputs;
+  postInstall = ''
+            wrapProgram $out/bin/too-many-cells \
+              --prefix 'PATH' ':' "${pkgs.Renv}/bin/" \
+              --prefix-contents 'R_LIBS_SITE' ':' "${pkgs.Renv}/bin/R" \
+              --set 'LANG' 'en_US.UTF-8' \
+              --set 'LOCALE_ARCHIVE' "${pkgs.glibcLocales}/lib/locale/locale-archive" \
+              --set 'FONTCONFIG_FILE' "${fontsConf}"
+            '';
+})))
