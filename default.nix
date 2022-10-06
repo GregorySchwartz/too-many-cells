34b9c9ee5695504824169f44e01d8c3ea2d55546
GregorySchwartz <gregory.schwartz@uhnresearch.ca>
2022-10-06 16:01:42 -0400

    Update for release.

--- default.nix
+++ default.nix
@@ -50,8 +50,7 @@ let
               && baseNameOf path != "dist")
             ./.;  # Filter out directories. Is recursive, but excluding a directory excludes all paths below it too.
     source-overrides = {
-      #birch-beer = builtins.fetchTarball https://github.com/GregorySchwartz/birch-beer/archive/54b1e54b07b267be3e9bf7ca8084a7fd8930a501.tar.gz;
-      birch-beer = ~/code/utility/birch-beer;
+      birch-beer = builtins.fetchTarball https://github.com/GregorySchwartz/birch-beer/archive/5f8123a9a0aced7b997630b55f640d26296ed2f2.tar.gz;
       hvega-theme = builtins.fetchTarball https://github.com/GregorySchwartz/hvega-theme/archive/66071f477e7a76d9cfbf0339dc4dc2d56c4c1748.tar.gz;
       ploterific = builtins.fetchTarball https://github.com/GregorySchwartz/ploterific/archive/ae02c2afad10d7ec9d05044021697d3e9036095f.tar.gz;
       sparse-linear-algebra = builtins.fetchTarball https://github.com/ocramz/sparse-linear-algebra/archive/dbad792f6c6a04e4de23806b676cb3e76d36a65b.tar.gz;
